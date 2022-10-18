894eca3cc1dee9a99dd75f762bf017af7ff4aba9
Vitor Dilucca <vitordilucca@hotmail.com>
2022-10-18 16:32:15 -0300

    512 views completado

--- vendor/MF/init/Bootstrap.php
+++ vendor/MF/init/Bootstrap.php
@@ -3,42 +3,41 @@
 namespace MF\Init;
 
 abstract class Bootstrap{
-
   private $routes;
 
-    public function __construct(){
-      $this->initRoutes();
-      $this->run($this->getUrl());
-    }
+  abstract protected function initRoutes();
 
-    public function getRoutes(){
-      return $this->routes;
-    }
+  public function __construct(){
+    $this->initRoutes();
+    $this->run($this->getUrl());
+  }
 
-    public function setRoutes(array $routes){
-      $this->routes = $routes;
-    }
+  public function getRoutes(){
+    return $this->routes;
+  }
 
-    protected function run($url) {
-      foreach ($this->getRoutes() as $key => $route) {
+  public function setRoutes(array $routes){
+    $this->routes = $routes;
+  }
 
-        if($url == $route['route']){
-          $class = "App\\Controllers\\".ucfirst($route['controller']);
-  
-          $controller = new $class();
-  
-          $action = $route['action'];
-  
-          $controller->$action();
-        }
+  protected function run($url) {
+    foreach ($this->getRoutes() as $key => $route) {
+      if($url == $route['route']){
+        $class = "App\\Controllers\\".ucfirst($route['controller']);
 
+        $controller = new $class();
+
+        $action = $route['action'];
+
+        $controller->$action();
       }
     }
+  }
 
-    protected function getUrl() {
-      return parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH);
-      // return parse_url('www.google.com/gmail?x10', PHP_URL_PATH);
-    }
+  protected function getUrl() {
+    return parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH);
+    // return parse_url('www.google.com/gmail?x10', PHP_URL_PATH);
+  }
 }
 
 ?>
