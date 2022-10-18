bbbb1ff2d6e03dc4dbcb5d2dc56f9932bdef0b83
Vitor Dilucca <vitordilucca@hotmail.com>
2022-10-18 15:09:16 -0300

    511 abstraçao da classe routes finalizado

--- App/Route.php
+++ App/Route.php
@@ -2,24 +2,11 @@
 
   namespace App;
 
-  class Route{
+  use MF\Init\Bootstrap;
 
-    private $routes;
-
-    public function __construct(){
-      $this->initRoutes();
-      $this->run($this->getUrl());
-    }
-
-    public function getRoutes(){
-      return $this->routes;
-    }
-
-    public function setRoutes(array $routes){
-      $this->routes = $routes;
-    }
-
-    public function initRoutes(){
+  class Route extends Bootstrap{
+  
+    protected function initRoutes(){
       
       $routes['home'] = array(
         'route' => '/',
@@ -34,27 +21,6 @@
       );
 
       $this->setRoutes($routes);
-    }
-
-    public function run($url) {
-      foreach ($this->getRoutes() as $key => $route) {
-
-        if($url == $route['route']){
-          $class = "App\\Controllers\\".ucfirst($route['controller']);
-  
-          $controller = new $class();
-  
-          $action = $route['action'];
-  
-          $controller->$action();
-        }
-
-      }
-    }
-
-    public function getUrl() {
-      return parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH);
-      // return parse_url('www.google.com/gmail?x10', PHP_URL_PATH);
     }
   }
 
